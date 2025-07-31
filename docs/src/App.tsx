67434ee5617e49c34ea86b1c27b875e7be5f4f18
haruna1256 <haru_kawagishi@icloud.com>
2025-07-31 16:07:25 +0900

    [add]

---
+++ docs/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Portfolio from "./Portfolio";
+import "./font.css";
+
+function App() {
+  return (
+    <div className="h-screen w-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
