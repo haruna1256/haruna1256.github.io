85d75929fc96026a3fc2ad339262e3b745bc34a1
haruna1256 <haru_kawagishi@icloud.com>
2025-07-31 16:13:44 +0900

    Deploy built files to docs

--- docs/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import type { UserConfigExport } from 'vite'
-
-export default defineConfig({
-  base: '/docs/',
-  plugins: [react()],
-} satisfies UserConfigExport)
