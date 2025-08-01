0392f5b111bcb47794243204f3b29d794592a7ab
haruna1256 <haru_kawagishi@icloud.com>
2025-08-01 11:00:15 +0900

    [remove] 使わないファイルの削除

--- docs/src/index.tsx
+++
@@ -1,6 +0,0 @@
-
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // TailwindCSS読み込み
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
