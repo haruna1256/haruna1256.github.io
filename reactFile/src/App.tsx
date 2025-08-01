4cd7bb7f93891201550ac3e2dc4d23e9ddaa7ca1
haruna1256 <haru_kawagishi@icloud.com>
2025-08-01 10:34:44 +0900

    [fix] react tailwind の環境を他フォルダで作成する

---
+++ reactFile/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-pink-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-pink-800">
+        Tailwind CSS が動いたよ 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
